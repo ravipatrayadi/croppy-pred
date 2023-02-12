303fdc666313646b8350d3dfef85879c951e24b4
ravipatrayadi <101858063+ravipatrayadi@users.noreply.github.com>
2023-02-12 22:37:18 +0530

    die

--- fakenewsdetect/urls.py
+++ fakenewsdetect/urls.py
@@ -19,24 +19,6 @@ from fakenews import views
 from django.conf.urls import url
 urlpatterns = [
     url('admin/', admin.site.urls),
-    url(r'^home', views.home, name='home'),
-	url(r'^nvb', views.nvb, name='nvb'),
-	url(r'^pac', views.pac, name='pac'),
-	url(r'^svm', views.svm, name='svm'),
-	url(r'^accuracy', views.accuracy, name='accuracy'),
-	url(r'^loginuser', views.loginuser, name='loginuser'),
-	url(r'^rf', views.rf, name='rf'),
-	url(r'^input', views.input, name='input'),
-	url(r'^test', views.test, name='test'),
-	url(r'^simple_upload', views.simple_upload, name='simple_upload'),
-	url(r'^simple', views.simple, name='simple'),
-	url(r'^fileshow', views.fileshow, name='fileshow'),
-	url(r'^fileshow1', views.fileshow1, name='fileshow1'),
 	url(r'^$', views.loginpage, name='loginpage'),
-	url(r'^register', views.register, name='register'),
-	url(r'^reg', views.reg, name='reg'),
-	url(r'^svr', views.svr, name='svr'),
-	url(r'^fertilizerRf', views.fertilizerRf, name='fertilizerRf'),
-	url(r'^fertilizerform', views.fertilizerform, name='fertilizerform'),
 	
 ]
