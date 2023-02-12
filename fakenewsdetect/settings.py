d80b84f3c0a7c613117420b067061f97044e5763
ravipatrayadi <101858063+ravipatrayadi@users.noreply.github.com>
2023-02-13 01:21:11 +0530

    ok

--- fakenewsdetect/settings.py
+++ fakenewsdetect/settings.py
@@ -120,6 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] # new
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # new
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'),] # new
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # new
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage' # new
