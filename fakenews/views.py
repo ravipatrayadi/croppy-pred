2662389f17f6ff67563c63a80ae50cc4aa14d86f
ravipatrayadi <101858063+ravipatrayadi@users.noreply.github.com>
2023-02-12 22:44:42 +0530

    sklean

--- fakenews/views.py
+++ fakenews/views.py
@@ -1,17 +1,9 @@
 # Libraries
 from django.shortcuts import render
 from django.http import HttpResponse
-import pandas as pd
-import emoji
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
 import itertools
-from sklearn.naive_bayes import MultinomialNB
-from sklearn import metrics
-from sklearn.linear_model import PassiveAggressiveClassifier
 import os
 
 import seaborn as sns
