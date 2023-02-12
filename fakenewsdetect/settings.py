c660fe9193afe53e3d541b6da5f6e17adbe05b5d
ravipatrayadi <101858063+ravipatrayadi@users.noreply.github.com>
2023-02-12 23:28:59 +0530

    check

--- fakenewsdetect/settings.py
+++ fakenewsdetect/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = ['*']
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Application definition
 
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
