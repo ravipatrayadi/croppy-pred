51a43eee4b0b9e1eab369c53f0f5b46868f8cf2b
ravipatrayadi <101858063+ravipatrayadi@users.noreply.github.com>
2023-02-13 00:09:55 +0530

    dd

--- fakenewsdetect/settings.py
+++ fakenewsdetect/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%4*%lel(x0u16a35w%j%4g#agea!c3ar#nkhkm=pp$_n9&mb!)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
